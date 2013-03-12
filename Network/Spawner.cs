ae72809b56008ba9b193921a310ac38a416d0623
Lance <lance@gamediy.net>
2013-03-12 17:31:02 +0800

    小调整

--- Network/Spawner.cs
+++ Network/Spawner.cs
@@ -18,16 +18,18 @@ public class Spawner : MonoBehaviour {
 
     void OnGUI(){
 
-        float refWidth = Screen.width;
-        float refHeight =Screen.height;
+        float refWidth = PlatformUtils.GetReferenceScreenResolution().x;
+        float refHeight = PlatformUtils.GetReferenceScreenResolution().y;
+
+        PlatformUtils.ScaleGUI();
 
         if(Network.peerType != NetworkPeerType.Disconnected && !Application.isLoadingLevel) {
-            if(GUI.Button(new Rect(refWidth / 2 - 140, refHeight / 2, 240, 60), "Ë¢¹Ö1")) {
+            if(GUI.Button(new Rect(refWidth / 2 - 220, refHeight - 100, 200, 80), "Ë¢¹Ö1")) {
                 Vector3 offset = new Vector3(Random.Range(-10, 10), 0, Random.Range(-50, 10));
                 Network.Instantiate(prefab1, transform.position + offset, Quaternion.identity, 0);
             }
 
-            if(GUI.Button(new Rect(refWidth / 2 + 140, refHeight / 2, 240, 60), "Ë¢¹Ö2")) {
+            if(GUI.Button(new Rect(refWidth / 2 + 20, refHeight - 100, 200, 80), "Ë¢¹Ö2")) {
                 Vector3 offset = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
                 Network.Instantiate(prefab2, transform.position + offset, Quaternion.identity, 0);
             }
